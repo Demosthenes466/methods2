930d3662565d6e2315733db0820acb6a6506b9dc
Tannor Ziehm <ziehmtj@s.dcsdk12.org>
2015-08-28 10:22:28 -0600

    Make sure to switch around the method

--- methods2.rb
+++ methods2.rb
@@ -1,8 +1,27 @@
 module Methods2
 	
 	# TODO - write elevenish?
+	def elevenish?(x)
+		if((x % 11 == 0) || (x % 11 == 1) && (x != 0))
+			return true
+		else
+			return false
+		end
+	end
+
+
 	
 	# TODO - write ice_cream_party?
+	def ice_cream_party?(ice_cream, candy)
+		if((ice_cream >= 5) & (candy >= 5))
+			return 1
+		elsif((ice_cream >= (2*candy)) || candy >= (2*ice_cream))
+			return 2
+		elsif((ice_cream < 5) || (candy < 5))
+			return 0
+		end
+	end
+
 	
 	# TODO - write successful_squirrel_party?
 	
